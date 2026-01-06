84eda7312c2db3678f9de4aede50a2836a301ea5
umamkhairul723 <umamkhairul723@gmail.com>
2026-01-06 14:09:58 +0700

    Menambahkan routing halaman

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'home');
+Route::view('/profil', 'profil');
+Route::view('/tata-tertib', 'tatib');
+Route::view('/daftar', 'daftar');
+
+Route::post('/daftar', function(Request $request){
+    session()->put('santri', $request->all());
+    return redirect('/hasil');
+});
+
+Route::get('/hasil', function(){
+    $santri = session('santri');
+    return view('hasil', compact('santri'));
 });
